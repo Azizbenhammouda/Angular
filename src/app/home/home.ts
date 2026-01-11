314c60ff4cfbc4580ae70addce2a18e6193ef39e
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-01-11 01:05:12 +0100

    HOOKS

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DataService } from '../data';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   imports: [],
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  users: any[] = [];
+  constructor(private dataService: DataService) { }
 
+  ngOnInit() {
+    this.dataService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+
+}
 }
